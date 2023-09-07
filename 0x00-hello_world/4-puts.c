d98c5ed6c05e21b48cbc2d2836503bdd441edfb4
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-07 14:56:28 -0700

    task 4 core

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-
 /**
  * main - Entry point of the program
  *
- * Description: This program prints a specific message
- * using the puts function and then returns 0.
+ * Description:
+ * This program prints the message "Programming is like building a multilingual puzzle,"
+ * followed by a newline character, using the `puts` function. It then returns 0 to indicate success.
  *
- * Return: Always 0 (success)
+ * Return: 0 for success
  */
 int main(void)
 {
-	/* Print the desired message */
-	puts("Programming is like building a multilingual puzzle");
-
-	/* Return 0 to indicate success */
-	return (0);
-}}
+    puts("Programming is like building a multilingual puzzle,");
+    return (0);
+}
