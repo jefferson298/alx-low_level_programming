600effd6ba217ac989e215b2fa839f213ddd0626
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-07 15:19:28 -0700

    task 4 C program print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 /* Use the puts function to print the message */
-puts("Programming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzle\n");
 /* Return 0 to indicate success */
 return (0);
 }
