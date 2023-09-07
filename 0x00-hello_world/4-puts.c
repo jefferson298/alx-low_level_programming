a639c25c441a7d600c83b5a1c065f75a40a4e425
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-07 15:43:30 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,8 +10,6 @@
  */
 int main(void)
 {
-/* Use the puts function to print the message */
 puts("\"Programming is like building a multilingual puzzle\n");
-/* Return 0 to indicate success */
 return (0);
 }
