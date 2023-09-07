7096a6064a9cd45d2ca5dda6143a3137d0d97d72
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-07 16:44:09 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,19 @@
  */
 int main(void)
 {
-printf("Size of char: %lu byte(s)\n", sizeof(char));
-printf("Size of int: %lu byte(s)\n", sizeof(int));
-printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of float: %lu byte(s)\n", sizeof(float));
-printf("Size of double: %lu byte(s)\n", sizeof(double));
-printf("Size of long double: %lu byte(s)\n", sizeof(long double));
+#if defined(__x86_64__) || defined(__ppc64__)
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+#else
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+#endif
 
 return (0);
 }
