b772d8404073aeac6a67f7a301075570a2c714ea
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-07 16:21:13 -0700

    task 5 C programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * This program prints the message "but the outcome is a piece of art,"
+ * Return is always : (0)
+ */
 int main(void)
 {
 printf("%s, but the outcome is a piece of art,\n", "with proper grammar");
