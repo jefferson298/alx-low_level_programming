e4629234bc686819a015a70e1cd86ed967bc5b06
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-07 14:48:10 -0700

    task 4 C Program without bin bash

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
-}
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a specific message
+ * using the puts function and then returns 0.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/* Print the desired message */
+	puts("Programming is like building a multilingual puzzle");
+
+	/* Return 0 to indicate success */
+	return (0);
+}}
