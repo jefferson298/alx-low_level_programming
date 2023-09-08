35953226112a141d84615c8761b2c2e47501316b
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-07 17:35:13 -0700

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,12 @@
 #include <unistd.h>
 /**
- * main
- *
- * Description:
- * Writes the message
- * "and that piece of art is useful" - Dora Korpar, 2015-10-19
- * continued by a newline to the standard error (stderr) and returns 1.
+ * main - writing a long message without  printf or put
  *
  * Return: 1 (indicating execution)
+ *
  */
 int main(void)
 {
-ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
-ssize_t bytes_written = write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
-if (bytes_written == len)
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
-else
-return (0);
 }
