d5d032c7ef3921d9de81bbf340eabf717fa8d527
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-08 17:59:07 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all letters except q and e
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	char alph = 'a';
+
+	while
+		(alph <= 'z');
+	{
+		if (alph != 'q' && alph != 'e')
+		{
+			putchar(alph);
+		}
+		alph++;
+	}
+	putchar('\n');
+	return (0);
+}
