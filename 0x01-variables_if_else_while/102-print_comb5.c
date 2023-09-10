47ddc730d4fd3cce67a2e97245848c2be45a2a83
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-10 10:12:56 -0700

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,32 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ * Description: prints numbers to in fours separed by comma
  *
  * Return: 0 (success)
  *
  */
+
 int main(void)
 {
-	int nums1, nums2, nums3, nums4;
+int num1, num2;
 
-	nums1, nums2, nums3, nums4 = 0;
-	while (nums1 <= '9')
-	{
-		while (nums2 <= '9')
-		{
-			while (nums3 <= '9')
-			{
-				while (nums4 <= '9')
-				{
-					if (((nums3 + nums4) > (nums1 + nums2) || nums1 < nums3)
-					{
-					putchar(nums1);
-					putchar(nums2);
-					putchar(' ');
-					putchar(nums3);
-					putchar(nums4);
-					if (nums1 + nums2 + nums3 + nums4 == '179' && nums1 == '9')
-					{
-					break;
-					}
-					else
-					{
-					putchar(',');
-					putchar(' ');
-					}
-				}
-
-				}
-				nums3++;
-				}
-			nums2++;
-		}
-		nums1++;
-	}
-	putchar('\n');
-	return (0);
+for (num1 = 0; num1 <= 98; num1++)
+{
+for (num2 = num1 + 1; num2 <= 99; num2++)
+{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+if (num1 != 98 || num2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
