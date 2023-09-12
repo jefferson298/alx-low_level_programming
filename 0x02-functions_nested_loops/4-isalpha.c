7608ffe1585236bd5f2ceba4e1972985cfdef63f
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-12 14:17:47 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _isalpha - return 1 when c is upper or lower
+ *
+ * Description: print using _putchar
+ *
+ * @c: get the character
+ *
+ * Return: 1 if c is upper or lower, if not 0
+ *
+ */
+int _isalpha(int c)
+{
+	char low, upper;
+	int alph = 0;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (low == 'c' || upper == 'C')
+			{
+				alph = 1;
+			}
+		}
+	}
+	return (alph);
+}
