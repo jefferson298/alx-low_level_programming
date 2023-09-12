c51d96753b602235b65f9825de16de53ea401e34
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-12 15:35:03 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - to print value sign
+ * Description: for n conditions
+ * @n: The n value
+ *
+ * Return: (0) or (1) mostly
+ *
+ */
+int print_sign(int n)
+{
+	int neg = -1;
+	char chr = (char) neg;
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (chr);
+	}
+}
