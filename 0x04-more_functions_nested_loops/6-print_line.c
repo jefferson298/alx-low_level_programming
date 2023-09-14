537a71c617014a4c2f5e127080d8e38be401ef59
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-14 13:35:31 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line -  Draws a straight line in the terminal
+ *@n: Number of times the line is printed
+
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a;
+
+		for (a = 1; a <= n; a++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
