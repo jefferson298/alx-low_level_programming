8145c190d2c33d8cb118ddeddd3fc156ce656377
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-18 08:43:37 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character
+ * followed by a new line.
+ * @str: The string input
+ *
+ * Return: 0 (success)
+ */
+void puts2(char *str)
+{
+	int li = 0;
+	int a = 0;
+char *b = str;
+int c;
+
+while (*b != '\0')
+{
+	b++;
+	li++;
+}
+a = li = 1;
+for (c = 0; c <= a; c++)
+{
+	if (c % 2 == 0)
+	{
+		_putchar(str[c]);
+	}
+}
+_putchar('\n');
+}
