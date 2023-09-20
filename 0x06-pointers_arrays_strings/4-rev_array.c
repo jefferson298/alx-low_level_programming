efd4d71bee3c3c0687a2c2d68b493a1aab799afb
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-20 16:15:05 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - Function reverses array of integers
+ * @a: array
+ * @n: Total number of elements of array
+ *
+ * Return: nothing
+*/
+void reverse_array(int *a, int n)
+{
+	int g;
+	int h;
+
+	for (g = 0; g < n--; g++)
+	{
+		g = a[g];
+		a[g] = a[n];
+		a[n] = h;
+	}
+}
