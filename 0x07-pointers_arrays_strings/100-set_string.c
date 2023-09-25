fa3faa4aefcc422ae805aa8bd4cbbcee6a83d285
jefferson298 <jeffersonmichael211@gmail.com>
2023-09-25 05:42:39 -0700

    advanced_task_100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_string - The source string that sets the value of
+ * the character
+ * @s: The pointer string
+ * @to: The character pointer
+ *
+ * Return: 0 (success)
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
