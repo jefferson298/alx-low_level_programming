c96fa77ff48cc2b63f32c1b36e78b2fa1787ce8f
jefferson298 <jeffersonmichael211@gmail.com>
2023-10-08 14:03:08 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked -  concatenates two strings & allocates memory using malloc
+ * @b: number of bytes
+ *
+ * Return: This returns a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *point;
+
+	point = malloc(b);
+
+	if (point == NULL)
+		exit(98);
+
+	return (point);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This is a 0x0C-more_malloc_free C programming task
