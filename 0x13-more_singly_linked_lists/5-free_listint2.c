52d6bcd23780c1dd98995341b1182248165e1910
jefferson298 <jeffersonmichael211@gmail.com>
2023-10-24 10:30:07 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - A function that frees a linked list
+ * @head: The pointer to the freed list
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+
+if (head == Nil)
+return;
+
+while (*head)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
+}
+
+*head = Nil;
+}
